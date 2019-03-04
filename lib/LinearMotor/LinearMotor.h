bcc073cd044c4541acb42820f6a18d5c89bded58
jaedle <dennis.jekubczyk@gmail.com>
2019-03-04 19:54:07 +0100

    refactoring

--- lib/LinearMotor/LinearMotor.h
+++ lib/LinearMotor/LinearMotor.h
@@ -7,19 +7,19 @@ class LinearMotor
 {
 public:
   LinearMotor(Relais *forward, Relais *backward);
+
   void forward();
   void backward();
-
   void stop();
 
   bool isMovingForward() { return movingForward; }
   bool isMovingBackward() { return movingBackward; }
-
   bool isStopped() { return !isMovingForward() && !isMovingBackward(); }
 
 private:
   Relais *relaisForward;
   Relais *relaisBackward;
+
   bool movingForward;
   bool movingBackward;
 };
